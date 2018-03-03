4f41b79bc6385437e8eb2635e5c7149383d2e243
Hunteerq <hunteerq@icloud.com>
2018-03-03 17:23:07 +0100

    First commit

---
+++ Household/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Household
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runProgram();
+
+        }
+        public static void runProgram()
+        {
+            Console.WriteLine("Witaj w programie obsługi wydatków, wybierz co chcesz zrobić?" );
+            Console.WriteLine("1.Dodaj rachunek\n2.Wyswietl statystyke\n3.Zakoncz");
+
+            try {
+                UserChoice = int.Parse(Console.ReadLine());
+            } catch ( ArgumentException E)
+            {
+                Console.WriteLine(E.Message);
+                UserChoice = int.Parse(Console.ReadLine()) ;
+            }
+        }
+
+
+
+        private static int _choice;
+        private static int UserChoice
+        {
+            get { return _choice; }
+            set
+            {
+                if (value == 1 || value == 2)
+                {
+                    _choice = value;
+                }
+                else if (value==3)
+                {
+                    Environment.Exit(2);
+                }
+                else
+                {
+                    throw new ArgumentException("Podaj wartosc 1, 2, lub 3");
+                }
+            }
+        }
+
+    }
+
+
+   
+}
